f0eb40e3c2de158da750430fe8c28cccfcf6eea1
Dunnigan <Shannon.Dunnigan@dep.state.fl.us>
2022-03-01 08:31:03 -0500

    initiated R folder with load-packages script

---
+++ R/00_load-packages.R
@@ -0,0 +1,27 @@
+# load packages -----------------------------------------------------------
+
+# be sure to check for packages conflicts!
+# import ----
+library(readxl) # read excel files
+library(janitor) # simple tools to clean dirty data
+library(here) # a simpler way to find your files
+library(xlsx)
+
+# tidy and wrangle ----
+library(tidyverse) # because...tidyverse (ggplot2, tidyr, dplyr)
+library(lubridate) # dates and times
+
+# stats ----
+library(broom) # convert statistical analysis objects into tidy tibbles
+library(psych) # has expanded statistical functions
+
+# markdown ----
+library(knitr)
+library(rmarkdown)
+library(kableExtra)
+
+# expanded data visualization ----
+library(khroma) # color-blind friendly color palettes for ggplot2
+library(patchwork) # grid graphics (better)
+library(scales) # scale functions for visualization
+library(plotly) # create interactive web graphics - use for html output files
